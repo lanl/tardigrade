8d6022a7a96b30e8f51b288d66c10c128e59550c
Nathan Miller <Nathan.A.Mill@gmail.com>
2018-04-26 16:05:20 -0600

    Initial commit of the internal couple residual

---
+++ include/kernels/InternalCouple.h
@@ -0,0 +1,81 @@
+/*!
+=====================================================================
+|                         InternalCouple.h                          |
+---------------------------------------------------------------------
+| The header file for the micromorphic internal couple kernel. This |
+| kernel draws upon the balance equations from the git repository   |
+| micromorphic_element available at:                                |
+|     https://bitbucket.org/NateAM2/                                |
+| and requires twelve degrees of freedom to be defined for solid    |
+| mechanics. These degrees of freedom are:                          |
+|     u_1, u_2, u_3, phi_11, phi_22, phi_33, phi_23, phi_13, phi,12 |
+|     phi_32, phi_31, phi_21                                        |
+| where u_1 -> u_3 are macro deformations, and phi_11 -> phi_21 are |
+| the components of the micro-displacement tensor.                  |
+=====================================================================
+*/
+
+
+#ifndef INTERNALCOUPLE_H
+#define INTERNALCOUPLE_H
+
+#include "Kernel.h"
+#include <balance_equations.h> //The Micromorphic balance equations
+
+//Forward declarations
+class InternalCouple;
+
+template <>
+InputParameters validParams<InternalCouple>();
+
+class InternalCouple : public Kernel{
+    /*!
+    ------------------------------------------
+    |    InternalCouple kernel definition    |
+    ------------------------------------------
+
+    We define the kernel for the internal force to be
+
+    psi (sigma_{ij} - s_{ij}) - -psi_{,k} m_{kji}
+
+    where psi is the test function, sigma is the cauchy stress, s is the 
+    symmetric stress, and m is the higher order couple stress. Note that 
+    this is a vector definition so we will need to define multiple kernels 
+    each obtaining a specific component of the residual.
+    */
+    public:
+        EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+        //The constructor definition
+        InternalCouple(const InputParameters & parameters);
+
+    protected:
+        //The residual at a quadrature point.
+        //Note: As stated above, a kernel is a scalar quantity so we have to define
+        //      as many of these as dimensions in our problem.
+        virtual Real computeQpResidual() override;
+        
+        //The diagonal members of the residual at a quadrature point.
+        //Note: The parameter chosen as the "diagonal" member is totally arbitrary
+        //      We just need to have each DOF associated with some residual term.
+        virtual Real computeQpJacobian() override;
+
+        //Parameters
+        const int _component_i;
+        const int _component_j;
+        const int _dof_num;
+
+        const MaterialProperty<std::vector<double>>              &_cauchy;           //The cauchy stress
+        const MaterialProperty<std::vector<double>>              &_s;                //The symmetric stress
+        const MaterialProperty<std::vector<double>>              &_m;                //The higher order couple stress
+        const MaterialProperty<std::vector<std::vector<double>>> &_DcauchyDgrad_u;   //The gradient of the cauchy stress w.r.t. u
+        const MaterialProperty<std::vector<std::vector<double>>> &_DcauchyDphi;      //The gradient of the cauchy stress w.r.t. the micro-deformation tensor
+        const MaterialProperty<std::vector<std::vector<double>>> &_DcauchyDgrad_phi; //The gradient of the cauchy stress w.r.t. the spatial gradient of the micro-deformation tensor
+        const MaterialProperty<std::vector<std::vector<double>>> &_DsDgrad_u;        //The gradient of the symmetric stress w.r.t. u
+        const MaterialProperty<std::vector<std::vector<double>>> &_DsDphi;           //The gradient of the symmetric stress w.r.t. the micro-deformation tensor
+        const MaterialProperty<std::vector<std::vector<double>>> &_DsDgrad_phi;      //The gradient of the symmetric stress w.r.t. the spatial gradient of the micro-deformation tensor
+        const MaterialProperty<std::vector<std::vector<double>>> &_DmDgrad_u;        //The gradient of the higher order couple stress w.r.t. u
+        const MaterialProperty<std::vector<std::vector<double>>> &_DmDphi;           //The gradient of the higher order couple stress w.r.t. the micro-deformation tensor
+        const MaterialProperty<std::vector<std::vector<double>>> &_DmDgrad_phi;      //The gradient of the higher order couple stress w.r.t. the spatial gradient of the micro-deformation tensor
+};
+
+#endif
---
+++ src/kernels/InternalCouple.C
@@ -0,0 +1,125 @@
+/*!
+=====================================================================
+|                         InternalCouple.h                          |
+---------------------------------------------------------------------
+| The header file for the micromorphic internal couple kernel. This |
+| kernel draws upon the balance equations from the git repository   |
+| micromorphic_element available at:                                |
+|     https://bitbucket.org/NateAM2/                                |
+| and requires twelve degrees of freedom to be defined for solid    |
+| mechanics. These degrees of freedom are:                          |
+|     u_1, u_2, u_3, phi_11, phi_22, phi_33, phi_23, phi_13, phi,12 |
+|     phi_32, phi_31, phi_21                                        |
+| where u_1 -> u_3 are macro deformations, and phi_11 -> phi_21 are |
+| the components of the micro-displacement tensor.                  |
+=====================================================================
+*/
+
+#include<InternalCouple.h>
+
+//We define the valid parameters for this kernel and their default values
+registerMooseObject("tardigrade", InternalCouple);
+
+template<>
+InputParameters
+validParams<InternalCouple>(){
+    InputParameters params = validParams<Kernel>();
+    params.addRequiredParam<int>("component_i", "The i component of the internal couple tensor");
+    params.addRequiredParam<int>("component_j", "The j component of the internal couple tensor");
+    params.addRequiredParam<int>("dof_num",   "The degree of freedom to use for the diagonal jacobian calculation");
+    return params;
+}
+
+InternalCouple::InternalCouple(const InputParameters & parameters)
+    : // We have to call the constructor for the base class first
+        Kernel(parameters),
+        _component_i(getParam<int>("component_i")),
+        _component_j(getParam<int>("component_j")),
+        _dof_num(getParam<int>("dof_num")),
+        _cauchy(getMaterialProperty<std::vector<double>>("cauchy")),
+        _s(getMaterialProperty<std::vector<double>>("s")),
+        _m(getMaterialProperty<std::vector<double>>("m")),
+        _DcauchyDgrad_u(getMaterialProperty<std::vector<std::vector<double>>>("DcauchyDgrad_u")),
+        _DcauchyDphi(getMaterialProperty<std::vector<std::vector<double>>>("DcauchyDphi")),
+        _DcauchyDgrad_phi(getMaterialProperty<std::vector<std::vector<double>>>("DcauchyDgrad_phi")),
+        _DsDgrad_u(getMaterialProperty<std::vector<std::vector<double>>>("DsDgrad_u")),
+        _DsDphi(getMaterialProperty<std::vector<std::vector<double>>>("DsDphi")),
+        _DsDgrad_phi(getMaterialProperty<std::vector<std::vector<double>>>("DsDgrad_phi")),
+        _DmDgrad_u(getMaterialProperty<std::vector<std::vector<double>>>("DmDgrad_u")),
+        _DmDphi(getMaterialProperty<std::vector<std::vector<double>>>("DmDphi")),
+        _DmDgrad_phi(getMaterialProperty<std::vector<std::vector<double>>>("DmDgrad_phi"))
+    {
+    /*!
+    =====================
+    |    Constructor    |
+    =====================
+
+    The constructor for the InternalForce class.
+    Note that this constructor is just variable 
+    assignments.
+
+    */
+}
+
+Real InternalCouple::computeQpResidual(){
+    /*!
+    ===========================
+    |    computeQpResidual    |
+    ===========================
+
+    Compute the residual at the quadrature point for 
+    the indicated component.
+
+    cint_ij = pse(sigma_ij - s_ij) -psi_k m_kji
+
+    where i = _component_i
+          j = _component_j
+
+    */
+    Real cint;
+    
+    //Copy the test function so that the balance equation function can read it
+    double dNdx[3];
+    //const double *p;
+    //p = &_grad_test[_i][_qp](0);
+    for (int i=0; i<3; i++){dNdx[i] = *(&_grad_test[_i][_qp](i));}//p+i);}
+    
+    balance_equations::compute_internal_couple(_component_i, _component_j, _test[_i][_qp], dNdx, 
+                                               _cauchy[_qp], _s[_qp], _m[_qp],
+                                               cint);
+    return cint;
+}
+
+Real InternalCouple::computeQpJacobian(){
+    /*!
+    ===========================
+    |    computeQpJacobian    |
+    ===========================
+
+    Compute the diagonal jacobian term.
+
+    */
+
+    Real dcdUint;
+
+    //Copy the test and interpolation functions so that the balance equation function can read it
+    double dNdx[3];
+    double detadx[3];
+    //const double *p;
+    //const double *__p;
+    //p   = &_grad_test[_i][_qp](0);
+    //__p = &_grad_phi[_i][_qp](0);
+    for (int i=0; i<3; i++){
+        dNdx[i]   = *(&_grad_test[_i][_qp](i));//p+i);
+        detadx[i] = *(&_grad_phi[_i][_qp](i));//__p+i);;
+    }
+
+
+    balance_equations::compute_internal_couple_jacobian(_component_i,  _component_j, _dof_num, 
+                                                        _test[_i][_qp], dNdx, _phi[_j][_qp], detadx,
+                                                        _DcauchyDgrad_u[_qp], _DcauchyDphi[_qp], _DcauchyDgrad_phi[_qp],
+                                                        _DsDgrad_u[_qp], _DsDphi[_qp], _DsDgrad_phi[_qp],
+                                                        _DmDgrad_u[_qp], _DmDphi[_qp], _DmDgrad_phi[_qp],
+                                                        dcdUint);
+    return dcdUint;
+}
